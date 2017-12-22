53abb2ea5f27b20805a20d89e616d168ebcd17fb
Cam Nguyen <cnguyen1187@gmail.com>
2017-12-22 20:52:04 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
+def countdown_with_sleep
+      x = 10
+      while x > 0
+      puts "#{number} SECOND(S)!"
+      x -= 1
+    end
+  return "HAPPY NEW YEAR!"
+end
 
 def countdown_with_sleep
       x = 10
